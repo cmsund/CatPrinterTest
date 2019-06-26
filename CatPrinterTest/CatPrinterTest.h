d43d420eff1f51cf908171cb08b0ffbe45712fe5
christinasund <christina.sund@tealium.com>
2019-06-26 11:02:10 -0700

    initial

---
+++ CatPrinterTest/CatPrinterTest.h
@@ -0,0 +1,19 @@
+//
+//  CatPrinterTest.h
+//  CatPrinterTest
+//
+//  Created by Christina Sund on 6/26/19.
+//  Copyright © 2019 Christina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CatPrinterTest.
+FOUNDATION_EXPORT double CatPrinterTestVersionNumber;
+
+//! Project version string for CatPrinterTest.
+FOUNDATION_EXPORT const unsigned char CatPrinterTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CatPrinterTest/PublicHeader.h>
+
+
---
+++ CatPrinterTestExample/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CatPrinterTestExample
+//
+//  Created by Christina Sund on 6/26/19.
+//  Copyright © 2019 Christina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var printer = CatPrinter()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        printer.meow()
+    }
+
+
+}
+
